cf66efd45690951fc8d5458893d5e2f3326df02c
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2021-04-30 01:58:26 +0900

    first_comitt

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const { App } = require('@slack/bolt');
-
+const { App, ExpressReceiver } = require('@slack/bolt');
+const serverlessExpress = require('@vendia/serverless-express');
 // Initializes your app with your bot token and signing secret
+
+const expressReceiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  // `processBeforeResponse` オプションは、あらゆる FaaS 環境で必須です。
+  // このオプションにより、Bolt フレームワークが `ack()` などでリクエストへの応答を返す前に
+  // `app.message` などのメソッドが Slack からのリクエストを処理できるようになります。FaaS では
+  // 応答を返した後にハンドラーがただちに終了してしまうため、このオプションの指定が重要になります。
+  processBeforeResponse: true
+});
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  receiver: expressReceiver
 });
 
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
   await say(`Hey there <@${message.user}>!`);
+
 });
 
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
-})();
+// Lambda 関数のイベントを処理します
+module.exports.handler = serverlessExpress({
+  app: expressReceiver.app
+});
